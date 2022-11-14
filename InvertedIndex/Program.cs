327889840e1553400bf7280fba1d7665da8e0dba
ANZX <anzx0@outlook.com>
2022-11-14 10:23:54 +0100

    feature: text read

--- InvertedIndex/Program.cs
+++ InvertedIndex/Program.cs
@@ -4,46 +4,94 @@
  * Program.cs
  */
 
+using System.Diagnostics.Tracing;
+using System.Text;
+using System.Text.RegularExpressions;
+
 public class InvertedIndex 
 {
     // вар
     public const int V = 34;
     
-    public const int COUNT_1 = 12500;  
-    public const int COUNT_2 = 50000; 
+    public const int COUNT_125 = 12500;  
+    public const int COUNT_500 = 50000; 
     
-    public const int STRAT_INDEX_1 = (COUNT_1 / 50) * (V - 1); 
-    public const int STRAT_INDEX_2 = (COUNT_2 / 50) * (V - 1); 
+    public const int START_INDEX_125 = (COUNT_125 / 50) * (V - 1); 
+    public const int START_INDEX_500 = (COUNT_500 / 50) * (V - 1); 
     
-    public const int STOP_INDEX_1 = (COUNT_1 / 50) * V; 
-    public const int STOP_INDEX_2 = (COUNT_2 / 50) * V;
+    public const int STOP_INDEX_125 = (COUNT_125 / 50) * V; 
+    public const int STOP_INDEX_500 = (COUNT_500 / 50) * V;
 
-    public static List<string> folder_paths_1 = new List<string>{ 
+    public const string QUERY_FORMAT = "{0}_*.txt";
+
+    public static List<string> folderPaths_125 = new List<string>{ 
         @"C:\Users\Anzx\MISC\SYNC\LABS\par_course_work\datasets\aclImdb\test\neg",
         @"C:\Users\Anzx\MISC\SYNC\LABS\par_course_work\datasets\aclImdb\test\pos",
         @"C:\Users\Anzx\MISC\SYNC\LABS\par_course_work\datasets\aclImdb\train\neg",
         @"C:\Users\Anzx\MISC\SYNC\LABS\par_course_work\datasets\aclImdb\train\pos"
     };
 
-    public static List<string> folder_paths_2 = new List<string>{
+    public static List<string> folderPaths_500 = new List<string>{
         @"C:\Users\Anzx\MISC\SYNC\LABS\par_course_work\datasets\aclImdb\train\unsup",
     };
 
     public static int Main(string[] args) 
     {
-        Console.WriteLine("Hello, World!");
+        //Console.WriteLine("Hello, World!");
 
-        foreach (string folder in folder_paths_1) 
+        foreach (string folder in folderPaths_125) 
         {
-            Map(folder);
+            Map(folder, START_INDEX_125, STOP_INDEX_125);
         }
 
         return 0;
     }
-    public static void Map(string folder_path) 
+
+    private static string GetTextByIndex(string folderPath, int index) 
     {
-        Console.WriteLine(folder_path);
+        string fileQuery = String.Format(QUERY_FORMAT, index);
+        
+        DirectoryInfo folder = new DirectoryInfo(folderPath);
+        FileInfo[] fileEntries = folder.GetFiles(fileQuery);
+        
+        FileInfo file = fileEntries[0];
+
+        if (!file.Exists)
+        {
+            throw new FileNotFoundException(String.Format("text with index {0} not found", index));
+        }
+
+        string text;
+
+        // Open the stream and read it.
+        using (StreamReader sr = file.OpenText())
+        {
+           text = sr.ReadToEnd();
+        }
+
+        return text;
     }
+
+    public static void Map(string folderPath, int startIndex, int stopIndex) 
+    {
+
+        for (int index = startIndex; index < stopIndex; index++)
+        {
+            string text = GetTextByIndex(folderPath, index);
+
+            text = Regex.Replace(text, @"[,]", "");
+            text = Regex.Replace(text, @"<br />", " <br/> ");
+
+            string[] words = text.Split(" ");
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                //Console.WriteLine(word);
+            }
+
+        }
+    }
+
     public void Reduce() 
     {
 
