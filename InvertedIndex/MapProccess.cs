088d281880cca5a271ff57a6fbc9fa67141465e2
ANZX <anzx0@outlook.com>
2022-11-18 16:39:34 +0100

    refactor

---
+++ InvertedIndex/MapProccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvertedIndex
+{
+    internal class MapProccess
+    {
+        private Thread thread;
+
+        public MapProccess()
+        {
+        }
+    }
+}
--- InvertedIndex/Program.cs
+++ InvertedIndex/Program.cs
@@ -10,102 +10,166 @@ using System.Diagnostics.Tracing;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace InvertedIndex 
+namespace InvertedIndex
 {
-    /// <summary>
-    /// Stores info of specific word regarding specific document 
-    /// </summary>
-    public class DocInfo
-    {
-        /// <summary>
-        /// document id
-        /// </summary>
-        public int docID;
+    ///// <summary>
+    ///// Stores info of specific word regarding specific document 
+    ///// </summary>
+    //public class DocInfo
+    //{
+    //    /// <summary>
+    //    /// document id
+    //    /// </summary>
+    //    public int docID;
 
-        /// <summary>
-        /// number of occurrences of word in a doc
-        /// </summary>
-        public int occurrNumber;
-        /// <summary>
-        /// count of word in a doc
-        /// </summary>
-        public int docLenght;
+    //    /// <summary>
+    //    /// number of occurrences of word in a doc
+    //    /// </summary>
+    //    public int occurrNumber;
+    //    /// <summary>
+    //    /// count of word in a doc
+    //    /// </summary>
+    //    public int docLenght;
 
-        /// <summary>
-        /// term frequency
-        /// </summary>
-        public double tf { get { return occurrNumber / docLenght; } }
+    //    /// <summary>
+    //    /// term frequency
+    //    /// </summary>
+    //    public double tf { get { return occurrNumber / docLenght; } }
 
-        /// <summary>
-        /// constructor
-        /// </summary>
-        /// <param name="docID">
-        /// <inheritdoc cref="DocInfo.docID" path="/summary"></inheritdoc>
-        /// </param>
-        /// <param name="occurrNumber">
-        /// <inheritdoc cref="DocInfo.occurrNumber" path="/summary"></inheritdoc>
-        /// </param>
-        /// <param name="docLenght">
-        /// <inheritdoc cref="DocInfo.docLenght" path="/summary"></inheritdoc>
-        /// </param>
-        public DocInfo(int docID, int occurrNumber, int docLenght)
-        {
-            this.docID = docID;
-            this.occurrNumber = occurrNumber;
-            this.docLenght = docLenght;
-        }
+    //    /// <summary>
+    //    /// constructor
+    //    /// </summary>
+    //    /// <param name="docID">
+    //    /// <inheritdoc cref="DocInfo.docID" path="/summary"></inheritdoc>
+    //    /// </param>
+    //    /// <param name="occurrNumber">
+    //    /// <inheritdoc cref="DocInfo.occurrNumber" path="/summary"></inheritdoc>
+    //    /// </param>
+    //    /// <param name="docLenght">
+    //    /// <inheritdoc cref="DocInfo.docLenght" path="/summary"></inheritdoc>
+    //    /// </param>
+    //    public DocInfo(int docID, int occurrNumber, int docLenght)
+    //    {
+    //        this.docID = docID;
+    //        this.occurrNumber = occurrNumber;
+    //        this.docLenght = docLenght;
+    //    }
 
-        /// <summary>
-        /// Combies two DocInfo with same <see cref="docID"></see>
-        /// </summary>
-        /// <param name="other"></param>
-        /// <returns>Combined DocInfo</returns>
-        /// <exception cref="InvalidOperationException">thrown when objects docIDs are different</exception>
-        private DocInfo Add(DocInfo other)
-        {
-            if (this.docID != other.docID)
-            {
-                throw new InvalidOperationException($"Objects docIDs are different: {this.docID} and {other.docID}");
-            }
+    //    /// <summary>
+    //    /// Combies two DocInfo with same <see cref="docID"></see>
+    //    /// </summary>
+    //    /// <param name="other"></param>
+    //    /// <returns>Combined DocInfo</returns>
+    //    /// <exception cref="InvalidOperationException">thrown when objects docIDs are different</exception>
+    //    private DocInfo Add(DocInfo other)
+    //    {
+    //        if (this.docID != other.docID)
+    //        {
+    //            throw new InvalidOperationException($"Objects docIDs are different: {this.docID} and {other.docID}");
+    //        }
 
-            this.occurrNumber += other.occurrNumber;
+    //        this.occurrNumber += other.occurrNumber;
 
-            return this;
-        }
+    //        return this;
+    //    }
 
-        public static DocInfo operator +(DocInfo left, DocInfo right)
-        {
-            return left.Add(right);
-        }
-    }
+    //    public static DocInfo operator +(DocInfo left, DocInfo right)
+    //    {
+    //        return left.Add(right);
+    //    }
+    //}
+    ///// <summary>
+    ///// Stores info of specific word
+    ///// </summary>
+    //public class WordInfo
+    //{
+    //    /// <summary>
+    //    /// Lexem
+    //    /// </summary>
+    //    public string word;
+
+    //    /// <summary>
+    //    /// List of documents where word occurrs
+    //    /// </summary>
+    //    public ConcurrentBag<DocInfo> docs = new ConcurrentBag<DocInfo>();
+
+    //    /// <summary>
+    //    /// IDF metric param
+    //    /// </summary>
+    //    /// <param name="N">total number of documents</param>
+    //    /// <returns>Inverse document frequency</returns>
+    //    public double GetIDF(int N)
+    //    {
+    //        return Math.Log(docs.Count / N);
+    //    }
+
+    //    public WordInfo(string word)
+    //    {
+    //        this.word = word;
+    //    }
+    //}
+
     /// <summary>
     /// Stores info of specific word
     /// </summary>
-    public class WordInfo
+    public class WordData
     {
         /// <summary>
-        /// Lexem
+        /// Stores info of specific document 
         /// </summary>
-        public string word;
-
-        /// <summary>
-        /// List of documents where word occurrs
-        /// </summary>
-        public ConcurrentBag<DocInfo> docs = new ConcurrentBag<DocInfo>();
-
-        /// <summary>
-        /// IDF metric param
-        /// </summary>
-        /// <param name="N">total number of documents</param>
-        /// <returns>Inverse document frequency</returns>
-        public double GetIDF(int N)
+        public class Document 
         {
-            return Math.Log(docs.Count / N);
+            /// <summary>
+            /// document id
+            /// </summary>
+            public readonly int id;
+
+            /// <summary>
+            /// number of occurrences of word in a document
+            /// </summary>
+            public int occurrsCount;
+
+            /// <summary>
+            /// count of words in a document
+            /// </summary>
+            public readonly int wordCount;
+
+            /// <summary>
+            /// term frequency
+            /// </summary>
+            public double tf { get { return occurrsCount / wordCount; } }
+
+            public Document(int id, int occurrsCount, int wordCount)
+            {
+                this.id = id;
+                this.occurrsCount = occurrsCount;
+                this.wordCount = wordCount;
+            }
         }
 
-        public WordInfo(string word)
+        /// <summary>
+        /// List of documents
+        /// </summary>
+        public List<Document> docs = new List<Document>();
+        
+        /// <summary>
+        /// Number of documents with this word
+        /// </summary>
+        public int docCount { get { return docs.Count; } }
+
+        /// <summary>
+        /// Inverse document frequency
+        /// </summary>
+        public double idf = 0;
+
+        public WordData() 
         {
-            this.word = word;
+        }
+
+        public void AddDocument(int id, int occurrsCount, int wordCount) 
+        {
+            var doc = new Document(id, occurrsCount, wordCount);
+            docs.Add(doc);
         }
     }
 
@@ -140,27 +204,28 @@ namespace InvertedIndex
 
         #endregion
 
-        private static ConcurrentDictionary<string, WordInfo> mapData = new ConcurrentDictionary<string, WordInfo>();
-        private static ConcurrentDictionary<string, WordInfo> reduceData = new ConcurrentDictionary<string, WordInfo>();
+        //private static ConcurrentDictionary<string, WordInfo> mapData = new ConcurrentDictionary<string, WordInfo>();
+        //private static ConcurrentDictionary<string, WordInfo> reduceData = new ConcurrentDictionary<string, WordInfo>();
 
-        public static int Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
+
             //Console.WriteLine("Hello, World!");
 
             foreach (string folder in folderPaths_125)
             {
-                Map(folder, START_INDEX_125, STOP_INDEX_125);
+                await IndexFilesAsync(folder, START_INDEX_125, STOP_INDEX_125);
             }
 
-            foreach (var word in mapData.Keys)
-            {
-                Reduce(mapData[word]);
-            }
+            //foreach (var word in mapData.Keys)
+            //{
+            //    Reduce(mapData[word]);
+            //}
 
             return 0;
         }
 
-        private static string GetDocByIndex(string folderPath, int index)
+        private static string GetDocContentByIndex(string folderPath, int index)
         {
             string fileQuery = String.Format(QUERY_FORMAT, index);
 
@@ -185,79 +250,156 @@ namespace InvertedIndex
             return text;
         }
 
-        public static void Map(string folderPath, int startIndex, int stopIndex)
+        public static async Task IndexFilesAsync(string folderPath, int startIndex, int stopIndex) 
         {
+            List<Task<Dictionary<string, WordData>>> mapTasks = new List<Task<Dictionary<string, WordData>>>();
+
             for (int docIndex = startIndex; docIndex < stopIndex; docIndex++)
             {
-                string text = GetDocByIndex(folderPath, docIndex);
+                string docContent = GetDocContentByIndex(folderPath, docIndex);
 
-                text = Regex.Replace(text, @"[,]", "");
-                text = Regex.Replace(text, @"<br />", " <br/> ");
+                var task = new Task<Dictionary<string, WordData>>(
+                    () => Map(docIndex, docContent));
+                
+                task.Start();
 
-                string[] words = text.Split(" ");
-
-                for (int i = 0; i < words.Length; i++)
-                {
-                    if (!words[i].Equals(String.Empty))
-                    {
-                        ProcessMap(words[i], docIndex, words.Length);
-                    }
-                }
-
+                mapTasks.Add(task);
+                
             }
+
+            foreach (var task in mapTasks) 
+            {
+                await task;
+            }
+
+            Console.WriteLine(mapTasks[0].Result);
         }
 
-        public static void ProcessMap(string word, int docID, int docLenght)
+        public static Dictionary<string, WordData> Map(int docId, string docContent) 
         {
-            if (!mapData.ContainsKey(word))
+            Console.WriteLine($"doc{docId} map started");
+
+            string cleanedText = Regex.Replace(docContent, @"[,]", "");
+            cleanedText = Regex.Replace(cleanedText, @"<br />", " <br/> ");
+
+            string[] words = cleanedText.Split(" ");
+            int wordsCount = words.Length;
+
+            Console.WriteLine($"doc{docId} words count: {wordsCount}");
+
+            Dictionary<string, WordData> mapData = new Dictionary<string, WordData>();
+
+            for (int i = 0; i < words.Length; i++)
             {
-                var newInfo = new WordInfo(word);
-                mapData.TryAdd(word, newInfo);
-            }
-
-            mapData.TryGetValue(word, out var info);
-            info.docs.Add(new DocInfo(docID, 1, docLenght));
-        }
-
-        public static void Reduce(WordInfo wordInfo)
-        {
-            if (!reduceData.ContainsKey(wordInfo.word))
-            {
-                reduceData.TryAdd(wordInfo.word, wordInfo);
-            }
-            else
-            {
-                reduceData.TryGetValue(wordInfo.word, out WordInfo storedInfo);
-
-                Dictionary<int, DocInfo> data = new Dictionary<int, DocInfo>();
-
-                foreach (var docInfo in storedInfo.docs)
+                if (words[i].Equals(String.Empty))
                 {
-                    if (data.ContainsKey(docInfo.docID))
-                    {
-                        data[docInfo.docID] += docInfo;
-                    }
-                    else
-                    {
-                        data.Add(docInfo.docID, docInfo);
-                    }
+                    continue;
                 }
 
-                foreach (var docInfo in wordInfo.docs)
-                {
-                    if (data.ContainsKey(docInfo.docID))
-                    {
-                        data[docInfo.docID] += docInfo;
-                    }
-                    else
-                    {
-                        data.Add(docInfo.docID, docInfo);
-                    }
-                }
+                string word = words[i];
 
-                storedInfo.docs = new ConcurrentBag<DocInfo>(data.Values);
+                if (mapData.TryGetValue(word, out WordData wd))
+                {
+                    wd.docs[0].occurrsCount += 1;
+                }
+                else
+                {
+                    wd = new WordData();
+                    wd.AddDocument(docId, 1, wordsCount);
+                    mapData.Add(word, wd);
+                }
             }
+
+            Console.WriteLine($"doc{docId} map finished");
+
+            return mapData;
         }
+
+        //public static void Map(string folderPath, int startIndex, int stopIndex)
+        //{
+        //    for (int docIndex = startIndex; docIndex < stopIndex; docIndex++)
+        //    {
+        //        string text = GetDocByIndex(folderPath, docIndex);
+
+        //        text = Regex.Replace(text, @"[,]", "");
+        //        text = Regex.Replace(text, @"<br />", " <br/> ");
+
+        //        string[] words = text.Split(" ");
+
+        //        Console.WriteLine($"words count {words.Length}");
+
+        //        for (int i = 0; i < words.Length; i++)
+        //        {
+        //            if (!words[i].Equals(String.Empty))
+        //            {
+        //                ProcessMap(words[i], docIndex, words.Length);
+        //            }
+        //        }
+
+        //    }
+        //}
+
+        //public static async Task<Dictionary<string, WordData>> MapAsync(int docIndex, string docContent) 
+        //{
+        //    var result = await Task<Dictionary<string, WordData>>.Run(() =>
+        //    {
+                
+        //    });
+
+        //    return result;
+        //}
+
+        //public static void ProcessMap(string word, int docID, int docLenght)
+        //{
+        //    if (!mapData.ContainsKey(word))
+        //    {
+        //        var newInfo = new WordInfo(word);
+        //        mapData.TryAdd(word, newInfo);
+        //    }
+
+        //    mapData.TryGetValue(word, out var info);
+        //    info.docs.Add(new DocInfo(docID, 1, docLenght));
+        //}
+
+        //public static void Reduce(WordInfo wordInfo)
+        //{
+        //    if (!reduceData.ContainsKey(wordInfo.word))
+        //    {
+        //        reduceData.TryAdd(wordInfo.word, wordInfo);
+        //    }
+        //    else
+        //    {
+        //        reduceData.TryGetValue(wordInfo.word, out WordInfo storedInfo);
+
+        //        Dictionary<int, DocInfo> data = new Dictionary<int, DocInfo>();
+
+        //        foreach (var docInfo in storedInfo.docs)
+        //        {
+        //            if (data.ContainsKey(docInfo.docID))
+        //            {
+        //                data[docInfo.docID] += docInfo;
+        //            }
+        //            else
+        //            {
+        //                data.Add(docInfo.docID, docInfo);
+        //            }
+        //        }
+
+        //        foreach (var docInfo in wordInfo.docs)
+        //        {
+        //            if (data.ContainsKey(docInfo.docID))
+        //            {
+        //                data[docInfo.docID] += docInfo;
+        //            }
+        //            else
+        //            {
+        //                data.Add(docInfo.docID, docInfo);
+        //            }
+        //        }
+
+        //        storedInfo.docs = new ConcurrentBag<DocInfo>(data.Values);
+        //    }
+        //}
     }
 }
 
