ce19274ae6de011fec023a3075218786e64c9d8f
ANZX <anzx0@outlook.com>
2022-11-23 18:11:46 +0100

    feature: TestFolderIndexingTime mathod added to InvertedIndexTests

--- SearchEngineTests/InvertedIndexTests.cs
+++ SearchEngineTests/InvertedIndexTests.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SearchEngine;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,19 @@ namespace SearchEngineTests
 {
     internal class InvertedIndexTests
     {
+        public static void TestFolderIndexingTime(string path, int startIndex, int stopIndex, int processCount) 
+        {
+            var stopwatch = new Stopwatch();
+            var indexer = new InvertedIndex();
+
+            stopwatch.Start();
+
+            indexer.IndexFolder(path, startIndex, stopIndex, processCount);
+
+            stopwatch.Stop();
+
+            Console.WriteLine(stopwatch.ElapsedTicks);
+        }
+
     }
 }
