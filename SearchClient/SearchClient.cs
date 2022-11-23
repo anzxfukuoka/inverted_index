b180aaaeb81dfff192338c5b52be42546d4ffefb
ANZX <anzx0@outlook.com>
2022-11-23 21:15:55 +0100

    feature: SerachClient Main rewrited

--- SearchClient/SearchClient.cs
+++ SearchClient/SearchClient.cs
@@ -17,41 +17,18 @@ namespace SearchClient
         private static string hostAddress = "127.0.0.1";
         private static int port = 1337;
 
-        public static int Main(string[] args)
-        {
-            Thread t = null;
-
-            for (int i = 0; i < 3; i++)
-            {
-               t  = new Thread(() => TestConn());
-               t.Start();
-            }
-
-            t.Join();
-
-            var kostil = Console.ReadLine();
-            return 0;
-        }
-
-        public static async void TestConn() 
+        public static async Task Main(string[] args)
         {
             Client client = new Client(hostAddress, port);
 
-            var searchQueries = new Queue<string>();
-
-            searchQueries.Enqueue("The");
-            searchQueries.Enqueue("Hadoop");
-            searchQueries.Enqueue("Good comedy movie");
-            searchQueries.Enqueue("Imagine back to when you were 11 years old.");
-            searchQueries.Enqueue("Vampire horror");
-
-            string queryResult = "";
-
-            while (searchQueries.TryDequeue(out string query))
+            while (true)
             {
+                Console.Write("query: ");
+                var query = Console.ReadLine();
+
                 Console.WriteLine($"Search query: {query}");
 
-                queryResult = await client.GetResponse(query);
+                var queryResult = await client.GetResponse(query);
 
                 Console.WriteLine($"Search result: \n{queryResult}");
             }
